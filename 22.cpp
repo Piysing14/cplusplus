0ab577da890a7d022817a386c3188f26c330d48f
Piyush Singh <khatipk14@gmail.com>
2024-10-05 00:15:43 +0530

    added the code

--- 22.cpp
+++ 22.cpp
@@ -1,1 +1,23 @@
 //22 .Write a Program to  Swap the Values of Two Variables Without Using any Extra Variable
+
+#include<iostream>
+using namespace std;
+    
+int main(){
+    int x,y;
+    cout<<"Enter x\n";
+    cin>>x;
+    cout<<"Enter y\n";
+    cin>>y;
+
+    cout<<"Before: "<<"x="<<x<<" y="<<y<<endl;
+
+    x = x + y;  
+    y = x - y;  
+    x = x - y;  
+
+    cout<<"After: "<<"x="<<x<<" y="<<y<<endl;
+
+    return 0;
+
+}
